826bb1dd227c2c31fadbc8e8b9ee748bc3d4bd66
DeekshitaV <deekshitaverma@gmail.com>
2021-09-23 22:54:54 +0530

    Initial commit

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    public VideoPlayer videoPlayer1;
+    public VideoPlayer videoPlayer2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer1.loopPointReached += activatePlayer2;
+        videoPlayer2.loopPointReached += changeScene;
+    }
+
+    void activatePlayer2(UnityEngine.Video.VideoPlayer vp)
+    {
+        videoPlayer2.Play();
+    }
+
+    void changeScene(UnityEngine.Video.VideoPlayer vp)
+    {
+
+        SceneManager.LoadScene("Login");
+    }
+
+}
+
+
+
