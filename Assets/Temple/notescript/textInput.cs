9b84e2ff964ec0b72c2ada59c0880ff5149028cf
Bhavyaa-Guptaa <bhavya122btcse20@igdtuw.ac.in>
2021-10-02 23:07:52 +0530

    tried audio

--- Assets/Temple/notescript/textInput.cs
+++ Assets/Temple/notescript/textInput.cs
@@ -15,55 +15,75 @@ namespace Temple
         public GameObject[] doors;
         public GameObject[] DoorImg;
 
-
+        public AudioSource Correct;
+        public AudioSource Incorrect;
 
         public void OnClickClue1()
         {
             if (inputField[0].text == answers[0])
             {
-
+                Correct.Play();
                 doors[0].SetActive(false);
                 DoorImg[0].SetActive(false);
                 inputField[0].text = "";
                 mouse.enabled = true;
                 Cursor.visible = false;
+              //  return;
+            }
+            else
+            {
+                Incorrect.Play();
             }
         }
         public void OnClickClue2()
         {
             if (inputField[1].text == answers[1])
             {
-
-
+                Correct.Play();
                 doors[1].SetActive(false);
                 DoorImg[1].SetActive(false);
                 inputField[1].text = "";
                 mouse.enabled = true;
                 Cursor.visible = false;
+              //  return;
+            }
+            else
+            {
+                Incorrect.Play();
             }
         }
         public void OnClickClue3()
         {
             if (inputField[2].text == answers[2])
             {
-
+                Correct.Play();
                 doors[2].SetActive(false);
                 DoorImg[2].SetActive(false);
                 inputField[2].text = "";
                 mouse.enabled = true;
                 Cursor.visible = false;
+              //  return;
+            }
+            else
+            {
+                Incorrect.Play();
             }
         }
         public void OnClickClue4()
         {
             if (inputField[3].text == answers[3])
             {
-
+                Correct.Play();
                 doors[3].SetActive(false);
                 DoorImg[3].SetActive(false);
                 inputField[3].text = "";
                 mouse.enabled = true;
                 Cursor.visible = false;
+            //    return;
+            }
+            else
+            {
+                Incorrect.Play();
             }
         }
 
