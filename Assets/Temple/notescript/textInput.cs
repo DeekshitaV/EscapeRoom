a321155fa21073e74f45c511f584da78c868a067
DeekshitaV <deekshitaverma@gmail.com>
2021-10-03 07:33:50 +0530

    corrected clue manager - TEMPLE

--- Assets/Temple/notescript/textInput.cs
+++ Assets/Temple/notescript/textInput.cs
@@ -17,26 +17,47 @@ namespace Temple
 
         public AudioSource Correct;
         public AudioSource Incorrect;
+        int door;
 
+        private void Start()
+        {
+            door = 0;
+        }
+
+        public void OnClickClue()
+        {
+            if (door == 0)
+                OnClickClue1();
+            else if (door == 1)
+                OnClickClue2();
+            else if (door == 2)
+                OnClickClue3();
+            else OnClickClue4();
+        }
         public void OnClickClue1()
         {
-            if (inputField[0].text == answers[0])
-            {
-                Correct.Play();
-                doors[0].SetActive(false);
-                DoorImg[0].SetActive(false);
-                inputField[0].text = "";
-                mouse.enabled = true;
-                Cursor.visible = false;
-              //  return;
+           
+                if (inputField[0].text == answers[0])
+                {
+                    Correct.Play();
+                    doors[0].SetActive(false);
+                    DoorImg[0].SetActive(false);
+                    inputField[0].text = "";
+                    mouse.enabled = true;
+                    Cursor.visible = false;
+                    door++;
+                //  return;
             }
-            else
-            {
-                Incorrect.Play();
-            }
+                else
+                {
+                    Incorrect.Play();
+                }
+                Debug.Log(door);
+               
         }
         public void OnClickClue2()
         {
+            
             if (inputField[1].text == answers[1])
             {
                 Correct.Play();
@@ -45,15 +66,19 @@ namespace Temple
                 inputField[1].text = "";
                 mouse.enabled = true;
                 Cursor.visible = false;
-              //  return;
-            }
+                door++;
+                //  return;
+             }
             else
             {
                 Incorrect.Play();
             }
+            Debug.Log(door);           
+            
         }
         public void OnClickClue3()
         {
+            
             if (inputField[2].text == answers[2])
             {
                 Correct.Play();
@@ -62,15 +87,19 @@ namespace Temple
                 inputField[2].text = "";
                 mouse.enabled = true;
                 Cursor.visible = false;
-              //  return;
+                door++;
+                //  return;
             }
             else
             {
                 Incorrect.Play();
             }
+            Debug.Log(door);     
+            
         }
         public void OnClickClue4()
         {
+           
             if (inputField[3].text == answers[3])
             {
                 Correct.Play();
@@ -79,15 +108,14 @@ namespace Temple
                 inputField[3].text = "";
                 mouse.enabled = true;
                 Cursor.visible = false;
-            //    return;
+                //    return;
             }
             else
             {
                 Incorrect.Play();
             }
-        }
-
-
+            Debug.Log(door);
+        }   
     }
 
 }
